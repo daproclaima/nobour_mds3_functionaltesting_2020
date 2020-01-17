0eecfb96fe5b0b31dcbfadf40212b8426c375f5b
DaProclaima <sn.itpro@gmail.com>
2020-01-17 17:01:42 +0100

    first tests ok

--- tests/uberTest.js
+++ tests/uberTest.js
@@ -1,11 +1,20 @@
-var conf = require('../conf/nightwatch.conf.js');
+//var conf = require('../conf/nightwatch.conf.js');
 
 module.exports = {
   'Open Website uber.com': function (browser) {
     browser
       .url('https://www.uber.com/fr/fr/')   // visit the url
       .waitForElementVisible('body', 5000) // wait for the body to be rendered
-      .assert.title('Uber – Augmentez vos revenus en conduisant ou commandez une course dès maintenant')
+      // .assert.title(Uber – Augmentez vos revenus en conduisant ou commandez une course dès maintenant"')
+      .assert.elementPresent('head')
+  },
+
+  'Check Block Login': function (browser) {
+    browser
+      .url('https://www.uber.com/fr/fr/')   // visit the url
+      .waitForElementVisible('body', 5000) // wait for the body to be rendered
+      .assert.elementPresent('*[id="/sign-in/"]')
+      .assert.elementPresent('*[id="/signup/"]')
   },
 };
 
